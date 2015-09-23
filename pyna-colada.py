d707649f28f53a7d7beff7c96dd946ab753a097b
etkirsch <etkirsch@gmail.com>
2015-09-23 09:34:04 -0700

    Now uses a json config file for settings. Initial work on authorized server list

--- pyna-colada.py
+++ pyna-colada.py
@@ -1,10 +1,11 @@
 import json, socket, requests, threading, time, sys
 
 class color:
-    header = '\033[95m'
+    header = '\033[96m'
     blue = '\033[94m'
     green = '\033[92m'
-    warning = '\033[93m'
+    gray = '\033[37m'
+    pyna_colada = '\033[93m'
     fail = '\033[91m'
     end = '\033[0m'
 
@@ -15,7 +16,7 @@ def color_print(message,printed_color):
 
 # Main client class
 class PynaClient(object):
-    def __init__(self, location, alias="anonymous"):
+    def __init__(self, location, alias):
         self.alias = alias
         self.active_server_list = []
         self.location = location
@@ -26,17 +27,17 @@ class PynaClient(object):
         if msg['type'] == 'whisper':
             # Format this differently
             color_print("{0} <W>: {1}".format(msg['sender']['name'], msg['message']),color.blue)
-        color_print("{0}: {1}".format(msg['sender']['name'], msg['message']),color.header)
+        color_print("{0}: {1}".format(msg['sender']['name'], msg['message']),color.gray)
 
     # Try to activate the server, or at least the alias
     def activate_server(self, location, alias):
         if location not in self.active_server_list:
             self.active_server_list.append(location)
-            color_print('ALERT: Activated {0}'.format(location),color.green)
+            color_print('CLIENT: Activated {0}'.format(location),color.green)
             self.connect_to(location)
         if alias not in self.active_aliases:
             self.active_aliases[alias] = location
-            color_print('ALERT: Registered {0} at {1}'.format(alias,location),color.green)
+            color_print('SERVER: Registered {0} at {1}'.format(alias,location),color.green)
 
     # Package up our data. Most of this will be in a json file, but we're not quite there yet
     def package(self,message_type,message=""):
@@ -51,7 +52,7 @@ class PynaClient(object):
     # Waits for input from the user, then sends it off to be handled
     def wait_for_input(self):
         while True:
-            chat = input('{0}@{1}:  '.format(self.alias,self.location))
+            chat = input('')
             client.handle_request(chat)
 
     # determines what to do with the string from wait_for_input
@@ -83,23 +84,29 @@ class PynaClient(object):
         total_sent = 0
         # create the socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.connect((address,int(port)))
-        while total_sent < len(message):
-            sent = self.sock.send(message[total_sent:])
-            if sent == 0:
-                raise RuntimeError('Connection closed erroneously')
-            total_sent = total_sent + sent
-        self.sock.close()
+        try:
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.sock.connect((address,int(port)))
+            while total_sent < len(message):
+                sent = self.sock.send(message[total_sent:])
+                if sent == 0:
+                    raise RuntimeError('Connection closed erroneously')
+                total_sent = total_sent + sent
+            self.sock.close()
+        except:
+            color_print('ERROR: Connection closed erroneously!!!',color.red)
+            pass
 
 
 # Main server clas
 class PynaServer(object):
-    def __init__(self, client, address='localhost',port=2008):
+    def __init__(self, client, address, port):
+        self.server_config = json.load(open('serverconfig.json','r'))
         self.authorized_server_list = []
         self.client = client
         self.address = address
         self.port = port
+        color_print('PyÑa Colada Server v{0}'.format(self.server_config['serverVersion']),color.pyna_colada)
 
     # This is the big part where we are waiting for messages
     def listen(self):
@@ -110,11 +117,11 @@ class PynaServer(object):
             self.sock.bind((self.address, self.port))
             self.sock.listen(1)
             self.client.sock = self.sock
-            color_print('Socket bound appropriately',color.green)
+            color_print('Server running on {0}:{1}\n'.format(self.address,self.port),color.green)
         except socket.error as msg:
             # No dice. Kill the process.
             self.sock = None
-            color_print('Unable to bind socket: {0}'.format(msg),color.fail)
+            color_print('ERROR: Unable to bind socket: {0}'.format(msg),color.fail)
             return
         while self.sock is not None:
             # Now we wait
@@ -128,8 +135,8 @@ class PynaServer(object):
         msg = json.loads(response.decode("utf-8"))
         if (msg['type'] == 'chat' or msg['type'] == 'whisper'):
             self.client.display(msg)
-        if msg['type'] == 'connect':
-            self.connect(msg['sender'])
+        #if msg['type'] == 'connection':
+        #    self.connect(msg['sender'])
         # Nonetheless, try to activate the server on the client
         self.client.activate_server(msg['sender']['location'],msg['sender']['name'])
 
@@ -137,22 +144,35 @@ class PynaServer(object):
     def connect(self, sender):
         if sender['location'] not in self.authorized_server_list:
             self.authorized_server_list.append(sender['location'])
+            with open('authorizedservers.json') as auth:
+                data = json.load(auth)
+            data.update({"authorizedServerList":self.authorized_server_list})
+            with open('authorizedservers.json','w') as auth:
+                json.dump(data, auth)
 
 
 
 
-# Set up
+# Get the local ip address
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 0))  # connecting to a UDP address doesn't send packets
+address = s.getsockname()[0]
+s.close()
+
+# Grab cmdline args and config.json
 alias = sys.argv[1]
-address = sys.argv[2]
-port = sys.argv[3]
+port = sys.argv[2]
 
+# Build the client
 client = PynaClient('{0}:{1}'.format(address,port),alias)
+
+# start the server up
 server = PynaServer(client,address,int(port))
 server_thread = threading.Thread(target=server.listen)
 server_thread.daemon = True
 server_thread.start()
 
-#Await initialization
+# Await initialization before starting client thread
 time.sleep(1)
 client_thread = threading.Thread(target=client.wait_for_input)
 client_thread.start()
--- send_chat.py
+++
@@ -1,8 +0,0 @@
-import json, requests, struct
-
-json = json.load(open('Examples/connect.json','r'))
-
-try:
-	requests.post('http://localhost:2008', json=json,headers={'Connection':'close'})
-except:
-	print('Connection closed')
