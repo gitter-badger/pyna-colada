a65dc4f1cdbf7dae694533c1dafe3da32863fc7a
etkirsch <etkirsch@gmail.com>
2015-09-22 17:42:54 -0700

    PinaServer now acts as a standalone process... but the client it uses is passed-by-value instead of by-reference...

--- pyna-collada.py
+++ pyna-collada.py
@@ -1,11 +1,12 @@
-import json, socket, requests, struct, codecs
+import json, socket, requests
+from multiprocessing import Process
+from multiprocessing.managers import BaseManager
 
 class PynaClient(object):
 	def __init__(self, location, alias="anonymous"):
-		self.authorized_server_list = []
 		self.alias = alias
-		self.location = location
 		self.active_server_list = []
+		self.location = location
 		self.active_aliases = {}
 
 	# show a message on the client
@@ -24,10 +25,17 @@ class PynaClient(object):
 		if alias not in self.active_aliases:
 			self.active_aliases[alias] = location
 			print('Registered {0} at {1}'.format(alias,location))
+		print('serverlist: {0}'.format(self.active_server_list))
+
+	def handle_request(self,message):
+		packed_json = self.package('chat',message)
+		self.send_chat(packed_json)
 
 	# Send JSON to all active servers
 	def send_chat(self,json):
+		print(self.active_server_list)
 		for active_server in self.active_server_list:
+			print(active_server)
 			request_receipt = requests.post('http://{0}'.format(active_server), json=json)
 
 	# Send JSON to a whisper target
@@ -41,10 +49,9 @@ class PynaClient(object):
 		data['type'] = message_type
 		data['client'] = 'Pyña colada'
 		data['client_version'] = 'v0.0.1'
-		data['sender'] = {'name': self.alias, 'location': self.server.address}
+		data['sender'] = {'name': self.alias, 'location': self.location}
 		data['message'] = message
 		return data
-
 
 class PynaServer(object):
 	def __init__(self, client, address='localhost',port=2008):
@@ -52,6 +59,15 @@ class PynaServer(object):
 		self.client = client
 		self.address = address
 		self.listen(port)
+
+	# Try to activate the server, or at least the alias
+	def activate_server(self, location, alias):
+		if location not in self.active_server_list:
+			self.active_server_list.append(location)
+			print('Activated {0}'.format(location))
+		if alias not in self.active_aliases:
+			self.active_aliases[alias] = location
+			print('Registered {0} at {1}'.format(alias,location))
 
 	# This is the big part where we are waiting for messages
 	def listen(self,port):
@@ -64,14 +80,16 @@ class PynaServer(object):
 		except socket.error as msg:
 			# No dice. Kill the process.
 			print('Unable to bind socket: {0}'.format(msg))
+			self.socket = None
 			return
 		self.socket.listen(1)
-		while 1:
+		while self.socket is not None:
 			# Now we wait
 			connection, address = self.socket.accept()
 			response = connection.recv(1024)
 			(headers, js) = response.decode('utf-8').split("\r\n\r\n")
 			self.receive(js)
+			connection.close()
 
 	# Handles receipt of the actual json we take in
 	def receive(self, message):
@@ -88,5 +106,9 @@ class PynaServer(object):
 		if sender['location'] not in self.authorized_server_list:
 			self.authorized_server_list.append(sender['location'])
 
-client = PynaClient('localhost')
-server = PynaServer(client)
+client = PynaClient('localhost:2008','neurotek')
+server = Process(target=PynaServer,args=(client,'localhost',2008))
+server.start()
+while True:
+	chat = input('')
+	client.handle_request(chat)
--- send_chat.py
+++ send_chat.py
@@ -2,4 +2,7 @@ import json, requests, struct
 
 json = json.load(open('Examples/whisper.json','r'))
 
-requests.post('http://localhost:2008', json=json)
+try:
+	requests.post('http://localhost:2008', json=json,headers={'Connection':'close'})
+except:
+	print('Connection closed')
