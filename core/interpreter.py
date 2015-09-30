1842cd8a7f69dd7ebdca3998eaa3db42b11c973a
etkirsch <etkirsch@gmail.com>
2015-09-30 08:37:02 -0700

    Improved clarity for interpreter

--- core/interpreter.py
+++ core/interpreter.py
@@ -8,35 +8,40 @@ class Interpreter(object):
 
 	# Handles receipt of the actual json we take in
 	def interpret_message(self,msg):
-		sender_location = msg['sender']['address']
+		sender = msg['sender']
 		# add this server to our list since we know it's real
-		self.check_node_status(msg['sender'])
+		self.check_node_status(sender)
 		# Determine what needs to be done according to the message type
 		if msg['type'] == 'ping':
-			self.processor.send("pingReply",sender_location)
+			self.processor.send("pingReply",sender['address'])
 			return
 		if msg['type'] == 'disconnection':
-			self.manager.deactivate_node(msg['sender'])
-			self.display.disconnected(msg['sender']['alias'],sender_location)
+			self.manager.deactivate_node(sender)
+			self.display.disconnected(sender['alias'],sender['address'])
 			return
 		if msg['type'] == 'connection':
-			self.processor.node_list_hash(sender_location)
+			self.processor.node_list_hash(sender['address'])
 			return
+		if 'nodeList' in msg['type']:
+			self.interpret_node_list(msg,sender)
+			return
+		if msg['type'] == 'whisper':
+			self.manager.most_recent_whisperer = sender['alias']
+		self.log_message(msg)
+		self.display.display(msg)
+
+	def interpret_node_list(self,msg,sender):
 		if msg['type'] == 'nodeListHash':
 			if not self.manager.hash_is_identical(msg['message']):
-				self.processor.full_node_list(sender_location)
+				self.processor.full_node_list(sender['address'])
 			return
 		if msg['type'] == 'nodeListFull':
 			diffed_nodes = self.manager.diff_node_list(msg['message'])
-			self.processor.node_list_diff(diffed_nodes,sender_location)
+			self.processor.node_list_diff(diffed_nodes,sender['address'])
 			return
 		if msg['type'] == 'nodeListDiff':
 			diffed_nodes = self.manager.add_nodes(msg['message'])
 			return
-		if msg['type'] == 'whisper':
-			self.manager.most_recent_whisperer = msg['sender']['alias']
-		self.log_message(msg)
-		self.display.display(msg)
 
 	def log_message(self,msg):
 		pass
